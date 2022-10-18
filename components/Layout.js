36e20f1d2a644002caf0fd926485e82365b94146
Michael <michael.laufersweiler@lulafit.com>
2022-10-18 11:22:03 -0500

    adding overall header, main, and footer layout

---
+++ components/Layout.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+
+
+export default function Layout({ title, children }) {
+    return (
+        <>
+            <Head>
+                <title>{title ? title + ' - NextJS Shop' : 'NextJS Shop'}</title>
+                <meta name="description" content="Ecommerce Website" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="flex min-h-screen flex-col justify-between ">
+                <header>
+                    <nav className="flex h-12 justify-between shadow-md items-center px-4">
+                        <Link href="/">
+                            <a className="text-lg font-bold">shopper</a>
+                        </Link>
+                        <div>
+                            <Link href="/card"><a className="p-2">Cart</a></Link>
+                            <Link href="/login"><a className="p-2">Login</a></Link>
+                        </div>
+                    </nav>
+                </header>
+                <main className="container m-auto mt-4 px-4">
+                    {children}
+                </main>
+                <footer className="flex justify-center items-center h-10 shadow-inner">
+                    <p>Copyright &copy; 2022 Shopper</p>
+                </footer>
+            </div>
+        </>
+    );
+}
