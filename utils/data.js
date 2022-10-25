0addd476c8ac144937c15285e0ffe1419a963639
Michael <michael.laufersweiler@lulafit.com>
2022-10-25 11:18:30 -0500

    add mongodb userbase connection

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Mike',
+            email: 'mlaufersweiler@gmail.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true
+        },
+        {
+            name: 'Mac',
+            email: 'user@gmail.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false
+        },
+    ],
     products: [
         {
             name: 'Free Shirt',
